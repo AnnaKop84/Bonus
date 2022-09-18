b24ccc0a7f2cd42bc3fa18a3056d22e74b929abe
Anna Kopeikina <karavay3@gmail.com>
2022-09-18 18:03:12 +0300

    add Bonus

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 3150;
+        int cash = 20;
+        int bonus = ticket/cash;
+        System.out.println("Вам начислено "+bonus+" миль");
+    }
+}
